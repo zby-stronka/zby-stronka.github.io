654b95960c1bad690e245b1fa2e545579eb59c61
zby-stronka <81350372+zby-stronka@users.noreply.github.com>
2021-04-02 14:53:24 +0200

    Add files via upload

--- Lekcja9/script.js
+++ Lekcja9/script.js
@@ -3,10 +3,15 @@
 // imgField -   obszar ze zdjeciem
 // 
   
-  // ustalenie stałych
-  const titleField = document.querySelector("h2");
+  // ustalenie zmiennych stałych
+  const titleField = document.querySelector("h4");
   const imgField = document.querySelector(".content");
   const img = document.createElement("img");
+
+//   ustalenie zmiennych zmiennych
+let currentSlide=0;
+// let changeSlide="left";
+
     // zdefiniowanie tablicy ze zdjęciami
   const views = [
        {"title": "Zachód słońca pod palmami",
@@ -21,11 +26,45 @@
    
 // strzałki -stałe
 const leftArr = document.querySelector(".left");
+leftArr.onclick = () => {changeSlide("left")};
 const rightArr = document.querySelector(".right");
+rightArr.onclick = () => {changeSlide("right")};
 
 
 // definicje funkcji
+function displayView(index){
+    titleField.innerHTML = views[index].title;
 
+    imgField.innerHTML = "";
 
+    img.src = "img/" + views[index].src
+    imgField.append(img);
+}
+function changeSlide(direction){
+    if (direction == "left"){
+        currentSlide--;
+        if (currentSlide < 0){
+            currentSlide = views.length-1;
+        }
+          
+    } else {
+        currentSlide++;
+        if (currentSlide > views.length-1){
+            currentSlide = 0;
+        
+        }
+                
+    }
+    displayView(currentSlide)
+}
 // wyświetlenie testowe
-console.log(views[1]);
+// console.log(views[3]);
+// displayView(0)
+// console.log(direction)
+// console.log(currentSlide)
+
+
+// wykonanie
+console.log(currentSlide);
+displayView(currentSlide);
+// changeSlide(currentSlide)
