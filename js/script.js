5a048bd88236a3ba5290bd3a43ed86f0e49fe81b
zby-stronka <81350372+zby-stronka@users.noreply.github.com>
2021-04-13 08:08:30 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// zegar
+function zegar()
+{
+ var dzien = dzisiaj.getDate();
+var miesiac = dzisiaj.getMonth()+1;
+var rok = dzisiaj.getFullYear();
+
+var godzina = dzisiaj.getHours();
+if (godzina<10) godzina = "0"+godzina;
+var minuta = dzisiaj.getMinutes();
+if  (minuta<10) minuta = "0"+minuta;
+var sekunda = dzisiaj.getSeconds();
+if  (sekunda<10) sekunda = "0"+sekunda;
+
+setTimeout("zegar"),1000;
+}
